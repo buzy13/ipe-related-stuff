24f14753f7e282d003769de540eff1b7121497a7
Marcel <radermacher@kit.edu>
2016-05-10 09:41:42 +0200

    moved to tools

--- tools/pdftoipe.sh
+++ tools/pdftoipe.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 echo $1
 inkscape --verb=ObjectToPath --select=EditSelectAll --export-plain-svg=/tmp/$1.svg $1
-svgtoipe.py /tmp/$1.svg $2
+./svgtoipe.py /tmp/$1.svg $2
 rm /tmp/$1.svg
